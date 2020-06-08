2613aacf4f25fbc3bedab97877c6b35a4844ed33
kili0 <52195771+kili0@users.noreply.github.com>
2020-06-08 14:24:47 +0900

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,11 +1,17 @@
- #!/usr/bin/env python3.6
- 
- import sys
+#!/usr/bin/env python3.6 
+import sys
+
 args = sys.argv
-t, c = sys.stdin.readline().strip().split("¥t") title, count = t, int(c)
+
+t, c = sys.stdin.readline().strip().split("¥t")
+title, count = t, int(c)
 for line in sys.stdin:
-t, c = line.strip().split("¥t") if title != t:
-if count > int(args[1]): print(title, "¥t", count);
-title, count = t, int(c) else:
-count += int(c) if count > int(args[1]):
-print(title, "¥t", count);
+  t, c = line.strip().split("¥t")
+  if title != t:
+   if count > int(args[1]):
+       print(title, "¥t", count);
+      title, count = t, int(c)
+  else:
+      count += int(c)
+if count > int(args[1]):
+    print(title, "¥t", count);
